65dc92e0805bf55add757a61e57dbb86a304f43c
Stefan Isaia <thenyartist@gmail.com>
2019-05-20 05:26:26 +0000

    Users connected to places

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,24 @@
+class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+def index
+  @places = Place.order("name").page(params[:page]).per_page(5)
+  @places.all
+end
+
+def new
+  @place = Place.new
+end
+
+def create
+  current_user.places.create(place_params)
+  redirect_to root_path
+end
+
+private
+
+def place_params
+  params.require(:place).permit(:name, :description, :address)
+end
+
+end
