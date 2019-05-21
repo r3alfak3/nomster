036325da7b166df2af250b21f0c00e8032eb0a3d
r3alfak3 <thenyartist@gmail.com>
2019-05-21 18:43:03 +0000

    Added some form validations to create, edit & update Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 def index
   @places = Place.order("name").page(params[:page]).per_page(5)
@@ -11,8 +11,12 @@ def new
 end
 
 def create
-  current_user.places.create(place_params)
-  redirect_to root_path
+  @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessible_entity
+  end
 end
 
 def show
@@ -25,16 +29,30 @@ def edit
   if @place.user != current_user
     return render plain: 'Not Allowed, please sign in to edit your own Places.', status: :forbidden
   end
+
 end
 
 def update
   @place = Place.find(params[:id])
+
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+
   @place.update_attributes(place_params)
-  redirect_to place_path(@place)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessible_entity
+  end
 end
 
 def destroy
   @place = Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed, please sign in to edit your own Places.', status: :forbidden
+  end
+
   @place.destroy
   redirect_to root_path
 end
