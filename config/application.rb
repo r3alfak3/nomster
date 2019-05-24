1158d6d60854f169a2d3f0258246b8a440c60cae
r3alfak3 <thenyartist@gmail.com>
2019-05-24 02:21:04 +0000

    Added User input validation & Geocoding

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module Nomster
     config.assets.paths << "#{Rails.root}/app/assets/fonts/"
 
     # Precompile additional assets
-    config.assets.precompile += %w( .svg .eot .woff .ttf )    
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_133439) do
+ActiveRecord::Schema.define(version: 2019_05_23_154314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_05_21_133439) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
@@ -31,13 +33,13 @@ ActiveRecord::Schema.define(version: 2019_05_21_133439) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
