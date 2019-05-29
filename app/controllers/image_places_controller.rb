9e66f33b0a3fe0be62e514f5d2acc53988df3102
r3alfak3 <thenyartist@gmail.com>
2019-05-29 06:11:09 +0000

    Problems adding picture associate with Place on the index page

--- app/controllers/image_places_controller.rb
+++ app/controllers/image_places_controller.rb
@@ -1,6 +1,11 @@
 class ImagePlacesController < ApplicationController
   before_action :authenticate_user!
  
+def index
+  @image_places = ImagePlace.find(params[:id])
+  @image_places.all
+end
+
   def create
     @place = Place.find(params[:place_id])
     @place.image_places.create(image_place_params.merge(user: current_user))
@@ -8,7 +13,6 @@ class ImagePlacesController < ApplicationController
   end
 
   private
-
   def image_place_params
     params.require(:image_place).permit(:caption, :imagePlace)
   end
--- app/models/image_place.rb
+++ app/models/image_place.rb
@@ -1,4 +1,7 @@
 class ImagePlace < ApplicationRecord
   belongs_to :user
   belongs_to :place
+  has_many :places
+
+  mount_uploader :imagePlace, ImagePlaceUploader
 end
